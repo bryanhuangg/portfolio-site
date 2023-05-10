256bb973f273d17f0608d900c44c3fed7acab288
bryan <bryanhuang66@gmail.com>
2023-05-10 02:02:10 -0700

    Fixed text animation jitter

--- components/section-transition.js
+++ components/section-transition.js
@@ -13,7 +13,7 @@ const SectionTransition = ({ children, delay = 0 }) => (
     <StyledDiv
         initial={{ y: -7, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.4, delay }}
+        transition={{ duration: 0.25, delay }}
         mb={6}
     >
         {children}
