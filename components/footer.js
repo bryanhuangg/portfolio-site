86499ed9db54bc30b9cdaaf2da553179afd4a33a
bryanhuangg <bryanhuangg@gmail.com>
2024-09-02 18:41:44 -0700

    fix footer font

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 
 const Footer = () => {
     return (
-        <Box align="center" opacity={0.4} fontSize="sm" fontFamily = "VT323" style={{ marginTop: '40px' }}>
+        <Box align="center" opacity={0.4} fontSize="xs" style={{ marginTop: '40px' }}>
             &copy; {new Date().getFullYear()} Bryan Huang. All Rights Reserved.
         </Box>
     )
