3fabe9196104da6fa20ce65a2169f8f504c1f896
bryanhuangg <bryanhuangg@gmail.com>
2024-12-25 00:48:58 -0800

    page innit and navbar routing

--- components/navbar.js
+++ components/navbar.js
@@ -1,27 +1,33 @@
 import React from 'react'
+import { useRouter } from 'next/router';
 import { Box, Button, Grid, Text } from "@chakra-ui/react"
 
 
 const Navbar = () => {
+    const router = useRouter();
     return (
         <Box borderBottom="1px" borderColor="gray.200" mt={4} p={1}>
             <Grid templateColumns="repeat(4, 1fr)" gap="6">
-                <Button variant="ghost">
+
+                <Button variant="ghost" onClick={() => router.push('/')}>
                     <Text textStyle={'subtitle'}>
                         Home
                     </Text>
                 </Button>
-                <Button variant="ghost">
+
+                <Button variant="ghost" onClick={() => router.push('/timeline')}>
                     <Text textStyle={'subtitle'}>
-                        Timeline
+                        CV Timeline
                     </Text>
                 </Button>
-                <Button variant="ghost">
+
+                <Button variant="ghost" onClick={() => router.push('/works')}>
                     <Text textStyle={'subtitle'}>
                         Work
                     </Text>
                 </Button>
-                <Button variant="ghost">
+
+                <Button variant="ghost" onClick={() => router.push('/posts')}>
                     <Text textStyle={'subtitle'}>
                         Posts
                     </Text>
---
+++ pages/posts.js
@@ -0,0 +1,31 @@
+import { Container, Box, Heading, Text, useColorModeValue, List, ListItem, Button } from '@chakra-ui/react'
+import styled from "@emotion/styled";
+import SectionTransition from "../components/section-transition";
+
+
+const Title = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+`
+
+const Body = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+`
+
+const Page = () => {
+    return (
+        <Container>
+            <SectionTransition delay={0}>
+                <Heading as="h3" marginTop={10} >
+                    <Title>
+                        <Text color = {useColorModeValue('#394867', '#f2f2f0')}>
+                            Posts
+                        </Text>
+                    </Title>
+                </Heading>
+            </SectionTransition>
+        </Container >
+    )
+}
+export default Page
