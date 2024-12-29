da1de909f3e2f491b3ed6113e7721ea1c775ac9d
bryanhuangg <bryanhuangg@gmail.com>
2024-12-29 00:19:05 -0800

    add more timeline events

---
+++ components/timeline-event.js
@@ -0,0 +1,73 @@
+import { Button, Image, Link, Text, useColorModeValue, useTheme } from '@chakra-ui/react'
+
+import { VerticalTimelineElement } from 'react-vertical-timeline-component'
+
+const TimelineEvent = ({
+    date,
+    logoSrc,
+    logoAlt,
+    title,
+    organization,
+    organizationUrl,
+    description,
+    showButton = false,
+    onButtonClick
+}) => {
+    const theme = useTheme();
+    const bgColor = useColorModeValue('#404258', '#F1F6F9')
+    const textColor = useColorModeValue(theme.colors.customDark, theme.colors.customLight)
+
+    return (
+        <VerticalTimelineElement
+            className="vertical-timeline-element"
+            contentStyle={{ background: bgColor }}
+            contentArrowStyle={{ borderRight: `5px solid ${bgColor}` }}
+            date={date}
+            iconStyle={{ background: '#f2f2f0' }}
+            icon={
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100%',
+                    height: '100%'
+                }}>
+                    <Image
+                        src={logoSrc}
+                        alt={logoAlt}
+                        width={25}
+                        height={25}
+                    />
+                </div>
+            }
+        >
+            <Text textStyle={'subtitle'} fontSize={'14px'} color={textColor}>
+                <b style={{ marginBottom: '-5px', display: 'block' }}>{title}</b>
+                <Link href={organizationUrl} target="_blank" style={{ display: 'inline-block' }}>
+                    {organization}
+                </Link>
+            </Text>
+
+            <Text textStyle={'text'} fontSize={'12px'} color={textColor}>
+                {description}
+                {showButton && (
+                    <>
+                        <br />
+                        <Button
+                            variant={'solid'}
+                            size={'xs'}
+                            color={bgColor}
+                            backgroundColor={textColor}
+                            mt={3}
+                            onClick={onButtonClick}
+                        >
+                            More details
+                        </Button>
+                    </>
+                )}
+            </Text>
+        </VerticalTimelineElement>
+    )
+}
+
+export default TimelineEvent
--- pages/timeline.js
+++ pages/timeline.js
@@ -18,6 +18,7 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 
 import { FaGithub } from "react-icons/fa";
 import SectionTransition from "../components/section-transition";
+import TimelineEvent from '../components/timeline-event';
 import styled from "@emotion/styled";
 
 const Page = () => {
@@ -41,130 +42,85 @@ const Page = () => {
                 <VerticalTimeline lineColor={useColorModeValue(theme.colors.customLight, theme.colors.customDark)} layout='2-columns'>
 
                     {/* MDA Space */}
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element"
-                        contentStyle={{ background: useColorModeValue('#404258', '#F1F6F9') }}
-                        contentArrowStyle={{ borderRight: `5px solid ${useColorModeValue('#404258', '#F1F6F9')}` }}
+                    <TimelineEvent
                         date="Jan 2024 - Dec 2024"
-                        iconStyle={{ background: '#f2f2f0' }}
-                        icon={
-                            <div style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                width: '100%',
-                                height: '100%'
-                            }}>
-                                <Image
-                                    src="/svg/mda.svg"
-                                    alt="MDA Space Logo"
-                                    width={25}
-                                    height={25}
-                                />
-                            </div>
-                        }>
-
-                        <Text textStyle={'subtitle'} fontSize={'14px'} color={useColorModeValue(theme.colors.customDark, theme.colors.customLight)}>
-                            <b style={{ marginBottom: '-5px', display: 'block' }}> Software Engineer Co-op </b>
-                            <Link href='https://mda.space/' target="_blank" style={{ display: 'inline-block' }}>MDA Space</Link>
-                        </Text>
-
-                        <Text textStyle={'text'} fontSize={'12px'} color={useColorModeValue(theme.colors.customDark, theme.colors.customLight)}>
-                            Developed software for large scale correlation and beam-forming as part of Canada’s contribution to the Square Kilometer Array (SKA),
-                            a global initiative to build the world’s next-largest telescope array.
-                            <br />
-                            <Button
-                                variant={'solid'}
-                                size={'xs'}
-                                color={useColorModeValue('#404258', '#F1F6F9')}
-                                backgroundColor={useColorModeValue(theme.colors.customDark, theme.colors.customLight)}
-                                mt={3}
-                            >
-                                More details
-                            </Button>
-                        </Text>
-                    </VerticalTimelineElement>
+                        logoSrc="/svg/mda.svg"
+                        logoAlt="MDA Space Logo"
+                        title="Software Engineer Co-op"
+                        organization="MDA Space"
+                        organizationUrl="https://mda.space/"
+                        description="Developed software for large scale correlation and beam-forming as part of Canada's 
+                                    contribution to the Square Kilometer Array (SKA), a global initiative to build the 
+                                    world's next-largest telescope array."
+                        showButton={true}
+                    />
 
 
                     {/* VSE */}
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element"
-                        contentStyle={{ background: useColorModeValue('#404258', '#F1F6F9') }}
-                        contentArrowStyle={{ borderRight: `5px solid ${useColorModeValue('#404258', '#F1F6F9')}` }}
+                    <TimelineEvent
                         date="Dec 2023 - Jun 2024"
-                        iconStyle={{ background: '#f2f2f0', color: '#394867' }}
-                        icon={<div style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            width: '100%',
-                            height: '100%'
-                        }}>
-                            <Image
-                                src="/svg/ubc.svg"
-                                alt="UBC Logo"
-                                width={26}
-                                height={26}
-                                mt={1}
-                            />
-                        </div>}
-                    >
-                        <Text textStyle={'subtitle'} fontSize={'14px'} color={useColorModeValue('#f2f2f0', '#394867')}>
-                            <b style={{ marginBottom: '-5px', display: 'block' }}>Web Designer</b>
-                            <Link href='https://economics.ubc.ca/' target="_blank" style={{ display: 'inline-block' }}>VSE at UBC</Link>
-                        </Text>
+                        logoSrc="/svg/ubc.svg"
+                        logoAlt="UBC Logo"
+                        title="Web Designer"
+                        organization="VSE at UBC"
+                        organizationUrl="https://economics.ubc.ca/"
+                        description="Contracted to design and build a WordPress website for the 
+                                    Cities in Motion research lab."
+                        showButton={true}
+                    />
 
-                        <Text textStyle={'text'} fontSize={'12px'} color={useColorModeValue('#f2f2f0', '#394867')}>
-                            Contracted to design and build a WordPress website for the Cities in Motion research lab.
-                            <br />
-                            <Button
-                                variant={'solid'}
-                                size={'xs'}
-                                color={useColorModeValue('#404258', '#F1F6F9')}
-                                backgroundColor={useColorModeValue('#f2f2f0', '#394867')}
-                                mt={3}
-                            >
-                                More details
-                            </Button>
-                        </Text>
-                    </VerticalTimelineElement>
-
+                    {/* Tetra Tech */}
+                    <TimelineEvent
+                        date="Aug 2023 - Dec 2023"
+                        logoSrc="/svg/tetratech.svg"
+                        logoAlt="Tetra Tech Logo"
+                        title="Fullstack Developer Co-op"
+                        organization="Tetra Tech"
+                        organizationUrl="https://www.tetratech.com/"
+                        description="Worked as a fullstack developer on FusionMap, a web-based GIS platform for
+                                    map-based data visualization and analysis."
+                        showButton={true}
+                    />
 
 
                     {/* CPSC TA */}
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element"
-                        contentStyle={{ background: useColorModeValue('#404258', '#F1F6F9') }}
-                        contentArrowStyle={{ borderRight: `5px solid ${useColorModeValue('#404258', '#F1F6F9')}` }}
+                    <TimelineEvent
                         date="Sep 2022 - Aug 2023"
-                        iconStyle={{ background: '#f2f2f0', color: '#394867' }}
-                        icon={<div style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            width: '100%',
-                            height: '100%'
-                        }}>
-                            <Image
-                                src="/svg/ubc.svg"
-                                alt="UBC Logo"
-                                width={26}
-                                height={26}
-                                mt={1}
-                            />
-                        </div>}
-                    >
-                        <Text textStyle={'subtitle'} fontSize={'14px'} color={useColorModeValue('#f2f2f0', '#394867')}>
-                            <b style={{ marginBottom: '-5px', display: 'block' }}> Computer Science TA </b>
-                            <Link href='https://economics.ubc.ca/' target="_blank" style={{ display: 'inline-block' }}>UBC CS Department</Link>
-                        </Text>
+                        logoSrc="/svg/ubc.svg"
+                        logoAlt="UBC Logo"
+                        title="Computer Science TA"
+                        organization="UBC CS Department"
+                        organizationUrl="https://www.cs.ubc.ca/"
+                        description="Teaching assistant for UBC's CPSC 110.
+                            Led weekly lab sessions and hosted office hours, providing instruction on concepts such as search algorithms, 
+                            graph theory, and recursion."
+                    />
 
-                        <Text textStyle={'text'} fontSize={'12px'} color={useColorModeValue('#f2f2f0', '#394867')}>
-                            Teaching assistant for UBC's CPSC 110.
-                            Led weekly lab sessions and hosted office hours to support students’ understanding of course material and
-                            provide individualized assistance.
-                        </Text>
-                    </VerticalTimelineElement>
+                    {/* Hillhouse Education */}
+                    <TimelineEvent
+                        date="Sep 2022 - Jul 2023"
+                        logoSrc="/svg/hillhouse.svg"
+                        logoAlt="UBC Logo"
+                        title="Private Tutor"
+                        organization="Hillhouse Education"
+                        organizationUrl="https://hillhouseeducation.com/"
+                        description="Provided academic support and guidance to high school students in mathematics, economics, and computer science through private tutoring sessions."
+
+                    />
+
+
+                    {/* UBC Notetaker */}
+                    <TimelineEvent
+                        date="Jan 2022 - Aug 2022"
+                        logoSrc="/svg/ubc.svg"
+                        logoAlt="UBC Logo"
+                        title="Notetaker"
+                        organization="UBC Centre for Accessibility"
+                        organizationUrl="https://students.ubc.ca/about-student-services/centre-for-accessibility/"
+                        description="Provided detailed notes for students in need of accommodations."
+
+                    />
+
                 </VerticalTimeline>
 
 
