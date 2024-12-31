27e8c9cba94562ae7cd1764001bbf992b08245d6
bryanhuangg <bryanhuangg@gmail.com>
2024-12-31 02:06:09 -0800

    finish hue card in works

--- pages/works.js
+++ pages/works.js
@@ -1,17 +1,8 @@
-import { Container, Card, CardBody, Box, Heading, Text, useColorModeValue, List, ListItem, Button } from '@chakra-ui/react'
-import styled from "@emotion/styled";
+import { Box, Button, Card, CardBody, Container, Flex, Heading, Image, Link, List, ListItem, Text, useColorModeValue } from '@chakra-ui/react'
+
+import { FaGithub } from "react-icons/fa";
 import SectionTransition from "../components/section-transition";
-
-
-const Title = styled.span`
-  font-size: 14px;
-  font-weight: 400;
-`
-
-const Body = styled.span`
-  font-size: 14px;
-  font-weight: 400;
-`
+import styled from "@emotion/styled";
 
 const Page = () => {
     return (
@@ -26,6 +17,44 @@ const Page = () => {
                             See my projects and products that I have worked on. These are a mix of personal projects, hackathons, and professional work.
                         </Text>
                     </CardBody>
+                </Card>
+            </SectionTransition>
+
+            <SectionTransition delay={0.1}>
+                <Card p={3}>
+                    <Flex direction={'row'}>
+                        <Image
+                            src='/images/works/hue-banner.png'
+                            w={'125px'}
+                            h={'200px'}
+                            borderRadius={8}
+                            boxShadow={'lg'}
+                        />
+                        <CardBody>
+                            <Flex direction={'column'} h={'100%'} justify={'space-between'}>
+                                <Box>
+                                    <Link href={'https://chromewebstore.google.com/detail/hue-more-colors-for-googl/gglmljnnfgfkajefpbgjaeobelpokhbn'} target='_blank'>
+                                        <Text textStyle={'subtitle'}> Hue: More Colors for Google Calendar</Text>
+                                    </Link>
+
+                                    <Text textStyle={'text'}>
+                                        A Chrome extension designed to enhance and personalize your Google Calendar experience,
+                                        currently with over 3000 active users.
+                                    </Text>
+                                </Box>
+
+                                <Flex direction={'row'} mt={4}>
+                                    <Button variant='solid' size={'sm'}>
+                                        See More
+                                    </Button>
+                                    <Button leftIcon={<FaGithub size={'16px'} />} variant='ghost' size={'sm'} onClick={() => window.open('https://github.com/bryanhuangg/gcal-hue', '_blank')}>
+                                        Source
+                                    </Button>
+                                </Flex>
+                            </Flex>
+                        </CardBody>
+                    </Flex>
+
                 </Card>
             </SectionTransition>
         </Container >
